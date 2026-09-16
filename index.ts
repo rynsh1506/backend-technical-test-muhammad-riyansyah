64fdef820cc64199875f074be14f5e5f3c65f5c5
xamnes <mriyansyah112@gmail.com>
2026-09-16 20:05:58 +0700

    feat: complete infrastructure with Dockerfile and Unit Test setup

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+
+export const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Inventory Procurement API is running!")
+  .listen(process.env.PORT || 3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
