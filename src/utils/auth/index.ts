f4cf6a6fa7deeee790806955839dfec591f8f3fa
xamnes <mriyansyah112@gmail.com>
2026-09-16 20:49:47 +0700

    fix(auth): use function plugin pattern for auth middleware to fix encapsulation bug

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -2,27 +2,28 @@ import { Elysia } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 import { APP_CONFIG } from "../../config";
 
-export const authSetup = new Elysia({ name: "authSetup" })
-  .use(
-    jwt({
-      name: "jwt",
-      secret: APP_CONFIG.JWT.SECRET,
-    })
-  )
-  .derive(({ jwt, cookie }) => {
-    return {
-      getCurrentUser: async () => {
-        const authToken = cookie[APP_CONFIG.COOKIE.NAME];
-        if (!authToken || !authToken.value) {
-          return null;
-        }
+export const authSetup = (app: Elysia) =>
+  app
+    .use(
+      jwt({
+        name: "jwt",
+        secret: APP_CONFIG.JWT.SECRET,
+      })
+    )
+    .derive(({ jwt, cookie }) => {
+      return {
+        getCurrentUser: async () => {
+          const authToken = cookie[APP_CONFIG.COOKIE.NAME];
+          if (!authToken || !authToken.value) {
+            return null;
+          }
 
-        const payload = await jwt.verify(authToken.value);
-        if (!payload || !payload.id) {
-          return null;
-        }
+          const payload = await jwt.verify(authToken.value);
+          if (!payload || !payload.id) {
+            return null;
+          }
 
-        return payload as { id: number; role: string; username: string };
-      },
-    };
-  });
+          return payload as { id: number; role: string; username: string };
+        },
+      };
+    });
