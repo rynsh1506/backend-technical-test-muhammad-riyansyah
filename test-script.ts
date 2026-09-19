414bc8abd62f1d5eed093f4be048acf41ee05bf0
xamnes <mriyansyah112@gmail.com>
2026-09-19 13:12:52 +0700

    chore: remove temporary script files

--- test-script.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./src/app";
-import { treaty } from "@elysiajs/eden";
-
-const api = treaty(app);
-
-const run = async () => {
-    type ProductPostPayload = Parameters<typeof api.products.post>[0];
-    const payload = { name: "Incomplete Product" } as unknown as ProductPostPayload;
-}
